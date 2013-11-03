790af6c6b4f01687e7a4e1fc5b77c9202a236f8c
amikushkina <allamikushkina@gmail.com>
2013-11-03 13:29:17 +0200

    1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: student
+ * Date: 11/3/13
+ * Time: 12:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
